eda5bdad26c87c1aa1af8a32569419e4abd892c6
mohit0101 <mohitjain2302@gmail.com>
2018-11-29 17:51:04 +0530

    fix link

---
+++ docs/precache-manifest.f0061480c7d46df61b5dc0f73bb4f616.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "33f0268bf1c648c7b536284e4b7b126f",
+    "url": "/next-3-days-plan-browser-homepage/static/media/setting-png.33f0268b.png"
+  },
+  {
+    "revision": "7b4bccb05c91a4145391",
+    "url": "/next-3-days-plan-browser-homepage/static/js/runtime~main.7b4bccb0.js"
+  },
+  {
+    "revision": "6e8325cb865f091e3cc6",
+    "url": "/next-3-days-plan-browser-homepage/static/js/main.6e8325cb.chunk.js"
+  },
+  {
+    "revision": "9e861a109ed9f1ba8ba1",
+    "url": "/next-3-days-plan-browser-homepage/static/js/1.9e861a10.chunk.js"
+  },
+  {
+    "revision": "6e8325cb865f091e3cc6",
+    "url": "/next-3-days-plan-browser-homepage/static/css/main.89a97c12.chunk.css"
+  },
+  {
+    "revision": "93435985e3d47ed9350c7c3902b71530",
+    "url": "/next-3-days-plan-browser-homepage/index.html"
+  }
+];
